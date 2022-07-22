0a84892be4512436ad0b7b9a276e62eb22626986
EGIN <EGIN@LAPTOP-JUH3P2T8>
2022-07-22 15:07:34 +0300

    22.07.2022

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -24,14 +24,25 @@ static void CategoryTest()
 static void ProductTest()
 {
     ProductManager productManager = new ProductManager(new EfProductDal());
+    var result = productManager.GetProductDetails();
+
+    //if (result.Success)
+    //{
+    //    foreach (var product in result.Data)
+    //    {
+    //        Console.WriteLine("{0} / {1}", product.ProductName, product.CategoryName);
+    //    }
+    //}
+    //else
+    //{
+    //    Console.WriteLine(result.Message);
+    //}
+    var result2 = productManager.Add(new Product { CategoryId = 5, ProductName = "a", UnitPrice = 12, UnitsInStock = 12 });
+    Console.WriteLine(result2.Message);
 
 
     //foreach (var product in productManager.GetAllByUnitPrice(20, 100))
     //{
     //    Console.WriteLine(product.ProductName);
     //}
-    foreach (var product in productManager.GetProductDetails())
-    {
-        Console.WriteLine("{0} / {1}", product.ProductName, product.CategoryName);
-    }
 }
---
+++ Core/Utilities/Results/Result.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.Results
+{
+    public class Result : IResult
+    {
+
+        public Result(bool success, string message) : this(success)
+        {
+            // this, classın kendisini kastediyor. this(success) = Result(success) gibi
+            // Readonly ler yani getler Constructor da set edilebilir.
+            Message = message;
+        }
+        public Result(bool success)
+        {
+            Success = success;
+        }
+
+        // get ler constructor içinde set edilebilir
+        public bool Success { get; }
+
+        public string Message { get; }
+    }
+}
